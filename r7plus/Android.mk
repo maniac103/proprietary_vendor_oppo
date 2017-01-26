a1263788f5f88745d9f0436b89392f749fe9aebb
Danny Baumann <dannybaumann@web.de>
2017-01-26 12:40:36 +0100

    r7sf, f1f: Move qcrilmsgtunnel to priv-app
    Change-Id: Ib3da1533f0dc6792b29148fd36c99c566ff7c11e

--- r7plus/Android.mk
+++ r7plus/Android.mk
@@ -45,11 +45,12 @@ include $(BUILD_PREBUILT)
 include $(CLEAR_VARS)
 LOCAL_MODULE := qcrilmsgtunnel
 LOCAL_MODULE_OWNER := oppo
-LOCAL_SRC_FILES := proprietary/app/qcrilmsgtunnel/qcrilmsgtunnel.apk
+LOCAL_SRC_FILES := proprietary/priv-app/qcrilmsgtunnel/qcrilmsgtunnel.apk
 LOCAL_CERTIFICATE := platform
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := APPS
 LOCAL_DEX_PREOPT := false
+LOCAL_PRIVILEGED_MODULE := true
 LOCAL_MODULE_SUFFIX := .apk
 include $(BUILD_PREBUILT)
 
